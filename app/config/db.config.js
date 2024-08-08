99d49454ccd34e6f48038f29c3cafa1bf4d89534
anchal <anchal@gmail.com>
2024-08-08 17:46:50 +0530

    users api implementation

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Anchal@0027",
+  DB: "testdb"
+};
---
+++ app/models/user.model.js
@@ -0,0 +1,129 @@
+const sql = require("./db.js");
+
+// constructor
+const User = function(user) {
+  this.name = user.name;
+  this.age = user.age;
+  this.published = user.published;
+};
+
+User.create = (newUser, result) => {
+  sql.query("INSERT INTO tutorials SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created user: ", { id: res.insertId, ...newUser });
+    result(null, { id: res.insertId, ...newUser });
+  });
+};
+
+User.findById = (id, result) => {
+  sql.query(`SELECT * FROM users WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found user: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Tutorial with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+User.getAll = (name, result) => {
+  let query = "SELECT * FROM users";
+
+  if (name) {
+    query += ` WHERE title LIKE '%${title}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("users: ", res);
+    result(null, res);
+  });
+};
+
+User.getAllPublished = result => {
+  sql.query("SELECT * FROM users WHERE published=true", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("users: ", res);
+    result(null, res);
+  });
+};
+
+User.updateById = (id, users, result) => {
+  sql.query(
+    "UPDATE users SET name = ?, age = ?, published = ? WHERE id = ?",
+    [tutorial.name, tutorial.age, tutorial.published, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Tutorial with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated tutorial: ", { id: id, ...user });
+      result(null, { id: id, ...user });
+    }
+  );
+};
+
+User.remove = (id, result) => {
+  sql.query("DELETE FROM Users WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Tutorial with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted user with id: ", id);
+    result(null, res);
+  });
+};
+
+User.removeAll = result => {
+  sql.query("DELETE FROM users", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} users`);
+    result(null, res);
+  });
+};
+
+module.exports = User;
